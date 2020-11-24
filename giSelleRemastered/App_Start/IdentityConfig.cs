e475caa3a908d597ea42335954fefdc1f0f903f0
JustBeYou <mihailferaru2000@gmail.com>
2020-11-24 17:06:11 +0200

    fix build

--- giSelleRemastered/App_Start/IdentityConfig.cs
+++ giSelleRemastered/App_Start/IdentityConfig.cs
@@ -53,11 +53,6 @@ namespace giSelleRemastered
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
             };
 
             // Configure user lockout defaults
